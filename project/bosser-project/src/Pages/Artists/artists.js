efb74239cacee20e95b778b48ff2cbc49239e56e
Ohad Yaakov <ohadyaakov1@gmail.com>
2021-05-29 22:04:09 +0300

    artists page design

--- project/bosser-project/src/Pages/Artists/artists.js
+++ project/bosser-project/src/Pages/Artists/artists.js
@@ -4,6 +4,8 @@ import Search from '../../components/Search/index'
 import artistLogo from "../Artists/artistLogo.jpg";
 import {Button} from "@material-ui/core";
 
+import ReactRoundedImage from "react-rounded-image"; // ------------------------
+
 class Artists extends Component{
 
     constructor(props) {
@@ -39,12 +41,23 @@ class Artists extends Component{
 
     create_artist(artist_id, idx) {
         return (<div key={idx}>
-            <div style={ {color: '#bab9b9' ,border: '3px solid #bab9b9', padding: '10px', margin: '14px', textAlign:'center', width: '24rem'} }>
-                <div>
-                    <img src={artistLogo} width="250" height="100"/>
+            {/*<div style={ {color: '#bab9b9' ,border: '3px solid #bab9b9', padding: '10px', margin: '14px', textAlign:'center', width: '24rem'} }>*/}
+            <div style={ {color: '#bab9b9', padding: '10px', margin: '14px', textAlign:'center', width: '24rem'} }>
+                <div style={ {position: "fixed", justifyContent: "center", alignItems: "center"} }>
+                    {/*<img src={artistLogo} width="250" height="100"/>*/}
+                    <ReactRoundedImage
+                        image={artistLogo}
+                        roundedColor="#66A5CC"
+                        imageWidth="150"
+                        imageHeight="150"
+                        roundedSize="13"
+                        borderRadius="150"
+                        hoverColor="#DD1144"
+                    />
+                    <br/>
                     <h3 style={ { color:'white'} }>{artist_id.full_name}</h3>
                     <br/>
-                    <Button id={'enterCourseButton'} style={ { color:'black' , border: '3px solid white' , background:'#c3c3c3'} }>כניסה לעמוד אמן</Button>
+                    {/*<Button id={'enterArtistButton'} style={ { color:'black' , border: '3px solid white' , background:'#c3c3c3'} }>כניסה לעמוד אמן</Button>*/}
                 </div>
             </div>
         </div>)
