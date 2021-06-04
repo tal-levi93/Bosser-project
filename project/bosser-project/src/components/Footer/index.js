3f936067645c40160d1edf8ed26cbc5f081c640c
tal-levi93 <79930240+tal-levi93@users.noreply.github.com>
2021-06-04 18:48:36 +0300

    newsletter done

--- project/bosser-project/src/components/Footer/index.js
+++ project/bosser-project/src/components/Footer/index.js
@@ -11,6 +11,8 @@ import {
 } from './Footer';
 import {useState} from 'react'
 import {SidebarContainer} from "../Header/Sidebar/SidebarElements";
+import firebase from "firebase";
+import validator from 'validator'
 
 class Footer extends Component {
 // const Footer = () => {
@@ -20,15 +22,37 @@ class Footer extends Component {
         this.state = {
             isOpen: false,
             email: "",
+            exist:"",
             setIsOpen: false
         }
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event) {
-        /*test - Tal you can delete it */
-        alert('A email was submitted: ' + this.state.email);
         event.preventDefault();
+        let exist;
+        if(validator.isEmail(this.state.email)){
+            let docRef = db.collection("newsletters").doc("wXWkD32q93M8YgTPJ4gh");
+            docRef.get().then((doc)=>{
+                let found = doc.data().Emails.find(element => element === this.state.email)
+                if(found == undefined){
+                    db.collection("newsletters").doc("wXWkD32q93M8YgTPJ4gh").update({
+                        Emails: firebase.firestore.FieldValue.arrayUnion(this.state.email)
+                    })
+                    alert("Signed in successfully")
+                }
+                else{
+                    alert("This email is aleardy on newsletter list")
+
+                }
+            }).catch((err)=>{
+                console.log(err)
+            })
+
+        }
+        else{
+            alert("This is not an email address , please try again")
+        }
     }
 
 
@@ -41,7 +65,6 @@ class Footer extends Component {
     handleChange = (e) =>{
         this.setState({
             [e.target.id]:e.target.value
-
         })
     }
 
@@ -55,8 +78,9 @@ class Footer extends Component {
                 <PopUp ipOpen={this.props.isOpen} toggle={this.props.toggle}>
                     <Form onSubmit={this.handleSubmit}>
 
-                        <label htmlFor="email"></label>
-                        <In id="email" value={this.state.email} onChange={this.handleChange} />
+                        <label  htmlFor="email"></label>
+                        <In id="email" value={this.state.email}   onChange={this.handleChange} />
+
                         {/*<In type="text" value={this.state.email} placeholder="הכנס דואר אלקטרוני" name="email " onfocusout={this.handleEmail}></In>*/}
                         <Confirm onClick={this.handleSubmit}>הרשמה</Confirm>
                         <Close onClick={this.props.toggle}>סגור</Close>
